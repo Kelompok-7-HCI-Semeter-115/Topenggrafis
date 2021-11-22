0caa2644c521cc23e6eb6209f6e8c84b20d28d81
Adnif <ahmadbani9@gmail.com>
2021-11-22 20:02:00 +0700

    menginstall file backend

---
+++ topenggrafis/order/forms.py
@@ -0,0 +1,21 @@
+from django import forms
+from django.forms import ModelForm
+from .models import Customer
+from django import forms
+
+class orderForm(ModelForm):
+    class Meta:
+        model = Customer
+        fields = [
+            'full_name',
+            'email',
+            'phone_number',
+            'services',
+        ]
+
+        widgets = {
+            'full_name':forms.TextInput(attrs={'class': 'form-control'}),
+            'email':forms.TextInput(attrs={'class': 'form-control'}),
+            'phone_number':forms.TextInput(attrs={'class': 'form-control'}),
+            'services':forms.Select(attrs={'class': 'form-control'}),
+        }
